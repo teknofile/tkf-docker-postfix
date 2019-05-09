7ea01d761908067670e24971920bfe27303053fc
teknofile <teknofile@teknofile.org>
2019-05-09 00:07:06 -0600

    a lot of clean up, still needs work. reading in some build confs from the filesystem instead of hard coding them

--- build.sh
+++ build.sh
@@ -6,9 +6,10 @@ docker create \
   -e PGID=952 \
   -e TZ=US/Mountain \
   -e TKF_MAILDOMAIN=copperdale.teknofile.net \
-  -e TKF_RELAY_HOST=email-smtp.us-west-2.amazonaws.com:587 \
-  -e TKF_MYNETWORKS="127.0.0.0/8 [::ffff:127.0.0.0]/104 [::1]/128 192.168.0.0/24 172.16.50.0/24 172.19.0.0/16" \
+  -e TKF_RELAY_HOST="$(cat ./tkf.relayhost)" \
+  -e TKF_MYNETWORKS="$(cat ./tkf.mynetworks)" \
   -e TKF_RELAY_PASSWORD="$(cat ./relay_password)" \
+  -e TKF_MYHOSTNAME="$(cat ./tkf.myhostname)" \
   -v /mnt/usb/docker/tkf-postfix/:/config \
   -p 25:25 \
   --restart unless-stopped \
